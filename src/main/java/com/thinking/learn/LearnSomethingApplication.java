e42ce2bda65a39e0e0bad8d5a935ac7ef6dc1665
lisi <si.li@thoughtworks.com>
2022-08-21 17:12:15 +0800

    init project

---
+++ src/main/java/com/thinking/learn/LearnSomethingApplication.java
@@ -0,0 +1,13 @@
+package com.thinking.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSomethingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSomethingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinking/learn/LearnSomethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinking.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSomethingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
